a61a6a894fb2aedbd2b1ea25723c07ff06df6202
Tadon <Mark.Klocek@GMail.com>
2024-08-09 15:52:31 -0700

    initialized oil type column c and d choices

--- oil_type_container.py
+++ oil_type_container.py
@@ -1,7 +1,7 @@
 import flet as ft
 import pics_and_desc
 oil_type_column_b_containers = []
-oil_type_selected_index = None
+oil_type_selected_index = 0
 oil_type_container = None
 
 def create_oil_type_section(page):
@@ -79,6 +79,7 @@ def create_oil_type_column_b_container(page):
     )
 
 def create_oil_type_column_b_row(page):
+    global oil_type_selected_index
     oil_type_column_b_containers = []
     for i in range(len(pics_and_desc.oil_type_column_b_pictures)):
         oil_type_column_b_containers.append(
@@ -89,19 +90,17 @@ def create_oil_type_column_b_row(page):
                 on_click=oil_type_columb_b_click(page, i)
             )
         )
-    return oil_type_column_b_containers
+    if oil_type_selected_index == 0:
+        oil_type_column_b_containers[0] = ft.Container(
+                content=create_oil_type_column_b_row_contents(page,i),
+                padding=5,
+                expand=True,
+                on_click=oil_type_columb_b_click(page,i),
+                bgcolor=ft.colors.ORANGE,
+                border=ft.Border(bottom=ft.BorderSide(10,ft.colors.ORANGE))
 
-def create_oil_type_column_b_row(page):
-    oil_type_column_b_containers = []
-    for i in range(len(pics_and_desc.oil_type_column_b_pictures)):
-        oil_type_column_b_containers.append(
-            ft.Container(
-                content=create_oil_type_column_b_row_contents(page, i),
-                padding=5,
-                expand=True,
-                on_click=oil_type_columb_b_click(page, i)
-            )
-        )
+        ) 
+    
     return oil_type_column_b_containers
 
 def create_oil_type_column_b_row_contents(page, i):
@@ -111,9 +110,9 @@ def create_oil_type_column_b_row_contents(page, i):
         
     )
 def create_oil_type_column_c_container(page):
-    return ft.Container(
+         return ft.Container(
         
-        content= ft.Text("Column C", color="Black",font_family="Roboto",weight=ft.FontWeight.BOLD),
+        content= ft.Text(pics_and_desc.oil_type_column_c_description[0], color="Black",font_family="Roboto",weight=ft.FontWeight.BOLD),
         bgcolor=ft.colors.ORANGE, 
         alignment=ft.alignment.center_left,
         padding=ft.padding.only(left=10),
@@ -126,7 +125,7 @@ def create_oil_type_column_c_container(page):
 def create_oil_type_column_d_container(page):
     return ft.Container(
         
-        content=ft.Text("Column d", color="Black", font_family="Roboto",weight=ft.FontWeight.BOLD),  
+        content=ft.Text(pics_and_desc.oil_type_column_d_description[0], color="Black", font_family="Roboto"),  
         bgcolor=ft.colors.ORANGE,
         alignment=ft.alignment.center_left,
         padding=ft.padding.only(left=10),
