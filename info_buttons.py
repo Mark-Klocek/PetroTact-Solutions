8e4fcb747674c0e87e86c5a9c8e5dd141459e5e2
Tadon <Mark.Klocek@GMail.com>
2024-10-03 15:43:56 -0700

    tactic natural recovery completed

--- info_buttons.py
+++ info_buttons.py
@@ -205,7 +205,7 @@ def treatment_tactic(page):
                                     ),
                                     width=content_window_width * 0.75,
                                     height=content_window_height,
-                                    border=ft.border.all(2,ft.colors.WHITE),
+                                    border=ft.Border(top=ft.BorderSide(2,ft.colors.WHITE),right=ft.BorderSide(2,ft.colors.WHITE), bottom=ft.BorderSide(2,ft.colors.WHITE)),
                                     bgcolor=tactic_info_column_bgcolor
                                 )
     
@@ -299,8 +299,13 @@ def content_window_tactic_name_column(page,tactic_info_container):
     content_window_width = window_width - (window_padding * 2)
     container_column = []
     container_column_row_height = content_window_height / 7
+    wm_border = None
     
     for i in range(7):
+        if i == 0:
+            wm_border = ft.Border(top=ft.BorderSide(2,ft.colors.WHITE))
+        if i == 6:
+            wm_border = ft.Border(bottom=ft.BorderSide(2,ft.colors.WHITE))
         container = ft.Container(
             content=ft.Row(
                 controls=[
@@ -326,7 +331,7 @@ def content_window_tactic_name_column(page,tactic_info_container):
             ),
             padding=5,
             bgcolor=generate_bgcolor(i),
-            #border=ft.border.all(1,ft.colors.RED),
+            border=wm_border,
             width=content_window_width,
             height=container_column_row_height,
             on_hover=change_tactic_name_window_bgcolor,
@@ -335,6 +340,7 @@ def content_window_tactic_name_column(page,tactic_info_container):
             )
         container_column.append(container)
     return container_column
+
 def generate_bgcolor(i):
     if i == global_variables.tactic_selected_variable:
         return "#E1E1E1"
@@ -389,13 +395,13 @@ def natural_recovery_info(page):
     content_window_height = window_height *0.9
     content_window_width = window_width - (window_padding * 2)
     column_array = []
-    text_size = content_window_height * 0.75 * 0.04
+    text_size = content_window_height * 0.75 * 0.035
     container = ft.Container(
         padding=0,
         width=content_window_width * 0.75,
         height=content_window_height * 0.45,
         alignment=ft.alignment.center,
-        border=ft.border.all(1, ft.colors.RED),
+        #border=ft.border.all(1, ft.colors.RED),
         content=ft.Image(src=r"images\Treatment Tactic\Treatment Tactic Large\treatment_tactic_large_natural_recovery.png",fit=ft.ImageFit.FIT_HEIGHT)
     )
     column_array.append(container)
@@ -404,73 +410,193 @@ def natural_recovery_info(page):
         padding=ft.padding.only(left=window_padding),
         width=content_window_width * 0.75,
         #height= content_window_height * 0.75,
-        border=ft.border.all(1,ft.colors.GREEN),
+        #border=ft.border.all(1,ft.colors.GREEN),
         content=ft.Text(
             spans=[
                 ft.TextSpan("Objective", style=ft.TextStyle(weight=ft.FontWeight.BOLD, size=text_size * 1.15, font_family="Roboto",color=ft.colors.BLACK) ),
                 ft.TextSpan("\n"),
-                ft.TextSpan("\n"),
+                #ft.TextSpan("\n"),
                 ft.TextSpan("Leave stranded oil to natural weathering and oil removal processes and allow the oiled shoreline to recover without intervention.",style=ft.TextStyle(size=text_size,font_family="Roboto",color=ft.colors.BLACK)),
-                ft.TextSpan("\n"),
+                #ft.TextSpan("\n"),
                 ft.TextSpan("\n"),
                 ft.TextSpan("\n"),
                 ft.TextSpan("Description", style=ft.TextStyle(weight=ft.FontWeight.BOLD, size=text_size * 1.15, font_family="Roboto",color=ft.colors.BLACK) ),
-                ft.TextSpan("\n"),
+                #ft.TextSpan("\n"),
                 ft.TextSpan("\n"),
                 ft.TextSpan("Evaluation of this option requires: knowledge of the oiling conditions; the coastal processes and physical character of the shoreline, and; the resources at risk, in order to evaluate the likely consequences of allowing the oil to be removed or degraded naturally. In many circumstances, it is appropriate to monitor the location to ensure that the assessment is correct or that the rate of weathering and natural oil removal proceeds as anticipated.",style=ft.TextStyle(size=text_size,font_family="Roboto",color=ft.colors.BLACK)),
-                ft.TextSpan("\n"),
+                #ft.TextSpan("\n"),
                 ft.TextSpan("\n"),
                 ft.TextSpan("\n"),
                 ft.TextSpan("Applications", style=ft.TextStyle(weight=ft.FontWeight.BOLD, size=text_size * 1.15, font_family="Roboto",color=ft.colors.BLACK) ),
-                ft.TextSpan("\n"),
+                #ft.TextSpan("\n"),
                 ft.TextSpan("\n"),
                 ft.TextSpan("Natural recovery can be applicable on any spill incident and for any type of coastal environment or shoreline type. Natural recovery is generally more applicable for:",style=ft.TextStyle(size=text_size,font_family="Roboto",color=ft.colors.BLACK)),
-                ft.TextSpan("\n"),
+                #ft.TextSpan("\n"),
                 ft.TextSpan("\n"),
                 ft.TextSpan("   •  small rather than large amounts of oil",style=ft.TextStyle(size=text_size,font_family="Roboto",color=ft.colors.BLACK)),
-                ft.TextSpan("\n"),
+                #ft.TextSpan("\n"),
                 ft.TextSpan("\n"),
                 ft.TextSpan("   •  non-persistant rather than persistent oil",style=ft.TextStyle(size=text_size,font_family="Roboto",color=ft.colors.BLACK)),
-                ft.TextSpan("\n"),
+                #ft.TextSpan("\n"),
                 ft.TextSpan("\n"),
                 ft.TextSpan("   •  exposed shorelines, rather than sheltered, low energy environments",style=ft.TextStyle(size=text_size,font_family="Roboto",color=ft.colors.BLACK)),
-                ft.TextSpan("\n"),
+                #ft.TextSpan("\n"),
                 ft.TextSpan("\n"),
                 ft.TextSpan("   •  remote or inaccessible areas",style=ft.TextStyle(size=text_size,font_family="Roboto",color=ft.colors.BLACK)),
                 ft.TextSpan("\n"),
                 ft.TextSpan("\n"),
                 ft.TextSpan("Selection of the natural recovery strategy may result from an evaluation which condludes that:",style=ft.TextStyle(size=text_size,font_family="Roboto",color=ft.colors.BLACK)),
-                ft.TextSpan("\n"),
+                #ft.TextSpan("\n"),
                 ft.TextSpan("\n"),
                 ft.TextSpan("   •  to treat or clean stranded oil may cause more damage than leaving the environment to recover naturally or",style=ft.TextStyle(size=text_size,font_family="Roboto",color=ft.colors.BLACK)),
-                ft.TextSpan("\n"),
+                #ft.TextSpan("\n"),
                 ft.TextSpan("\n"),
                 ft.TextSpan("   •  response techniques cannot accelerate natural recovery or",style=ft.TextStyle(size=text_size,font_family="Roboto",color=ft.colors.BLACK)),
-                ft.TextSpan("\n"),
+                #ft.TextSpan("\n"),
                 ft.TextSpan("\n"),
                 ft.TextSpan("   •  safety considerations could place response personnel in danger either from the oil (itself) or from environmental conditions (weather, access, hazards, etc.)",style=ft.TextStyle(size=text_size,font_family="Roboto",color=ft.colors.BLACK)),
+                ft.TextSpan("\n"),
+                ft.TextSpan("\n"),
+                ft.TextSpan("Natural recovery should always be considered as the preferred option; particularly for small amounts of oil. The trade-off or net environmental benefit for each segment typically considers:",style=ft.TextStyle(size=text_size,font_family="Roboto",color=ft.colors.BLACK)),
+                #ft.TextSpan("\n"),
+                ft.TextSpan("\n"),
+                ft.TextSpan("   •  the preficted fate and persistence of the residual oil",style=ft.TextStyle(size=text_size,font_family="Roboto",color=ft.colors.BLACK)),
+                #ft.TextSpan("\n"),
+                ft.TextSpan("\n"),
+                ft.TextSpan("   •  the estimated rate of natural recovery",style=ft.TextStyle(size=text_size,font_family="Roboto",color=ft.colors.BLACK)),
+                #ft.TextSpan("\n"),
+                ft.TextSpan("\n"),
+                ft.TextSpan("   •  the possible benefits of a response to accelerate recovery",style=ft.TextStyle(size=text_size,font_family="Roboto",color=ft.colors.BLACK)),
+                #ft.TextSpan("\n"),
+                ft.TextSpan("\n"),
+                ft.TextSpan("   •  the risks associated with the presence of the oil as it weathers",style=ft.TextStyle(size=text_size,font_family="Roboto",color=ft.colors.BLACK)),
+                #ft.TextSpan("\n"),
+                ft.TextSpan("\n"),
+                ft.TextSpan("   •  the possible delays to recovery that may be caused by response activities",style=ft.TextStyle(size=text_size,font_family="Roboto",color=ft.colors.BLACK)),
             ]
         )
         
     )
     column_array.append(container)
     container = ft.Container(
-        padding=0,
+        padding=window_padding,
         width=content_window_width * 0.75,
-        height= content_window_height * 0.15,
-        border=ft.border.all(1, ft.colors.BLUE)
+        height= content_window_height * 0.25,
+        #border=ft.border.all(1, ft.colors.BLUE),
+        content=ft.Column(
+            spacing=0,
+            expand=True,
+            controls=[
+                ft.Container(
+                    padding=0,
+                    bgcolor=ft.colors.TRANSPARENT,
+                    content=ft.Text("Summary of Effeciency Factors for Natural Recovery", size=text_size,style=ft.TextStyle(italic=True),color="Black",font_family="Roboto"),
+                    width=content_window_width * 0.75,
+                    alignment=ft.alignment.center,
+                    
+                ),
+                ft.Container(
+                    padding=0,
+                    bgcolor="#B8B8C7",
+                    border_radius=ft.border_radius.only(top_left=15,top_right=15),
+                    height=content_window_height * 0.09,
+                    width= content_window_width * 0.75,
+                    content=ft.Row(
+                        spacing=0,
+                        controls=[
+                            ft.Container(
+                                padding=0,
+                                #border=ft.border.all(1, ft.colors.BLUE),
+                                width=content_window_width * 0.75 /5 * 0.90 -1,
+                                border_radius=ft.border_radius.only(top_left=15),
+                                content=ft.Text("Technique", weight=ft.FontWeight.BOLD,color="White",font_family="Roboto",text_align=ft.TextAlign.CENTER,size=text_size)
+                            ),
+                            ft.Container(
+                                padding=0,
+                                #border=ft.border.all(1, ft.colors.BLUE),
+                                width=content_window_width * 0.75 /5 -1,
+                                content=ft.Text("Resource Requirements", weight=ft.FontWeight.BOLD,color="White",font_family="Roboto",text_align=ft.TextAlign.CENTER,size=text_size)
+                            ),
+                            ft.Container(
+                                padding=0,
+                                #border=ft.border.all(1, ft.colors.BLUE),
+                                width=content_window_width * 0.75 /5 -1,
+                                content=ft.Text("Relative Cleanup \n Rate", weight=ft.FontWeight.BOLD,color="White",font_family="Roboto",text_align=ft.TextAlign.CENTER,size=text_size)
+                            ),
+                            ft.Container(
+                                padding=0,
+                                #border=ft.border.all(1, ft.colors.BLUE),
+                                width=content_window_width * 0.75 /5 -1,
+                                content=ft.Text("Single or \n Multiple Step", weight=ft.FontWeight.BOLD,color="White",font_family="Roboto",text_align=ft.TextAlign.CENTER,size=text_size)
+                            ),
+                            ft.Container(
+                                padding=0,
+                                #border=ft.border.all(1, ft.colors.BLUE),
+                                width=content_window_width * 0.75 /5 * 0.95 -1,
+                                border_radius=ft.border_radius.only(top_right=15),
+                                content=ft.Text("Waste Generation", weight=ft.FontWeight.BOLD,color="White",font_family="Roboto",text_align=ft.TextAlign.CENTER,size=text_size)
+                            )
+                        ]
+                    )
+                ),
+                ft.Container(
+                    padding=0,
+                    bgcolor="#FFFFFF",
+                    border_radius=ft.border_radius.only(bottom_left=15,bottom_right=15),
+                    height=content_window_height * 0.09,
+                    width= content_window_width * 0.75,
+                    content=ft.Row(
+                        spacing=0,
+                        controls=[
+                            ft.Container(
+                                padding=0,
+                                border=ft.Border(right=ft.BorderSide(2, "#B8B8C7")),
+                                width=content_window_width * 0.75 /5 * 0.90 ,
+                                content=ft.Text("Natural Recovery",color="Black",font_family="Roboto",size=text_size,text_align=ft.TextAlign.CENTER,weight=ft.FontWeight.BOLD)
+                            ),
+                            ft.Container(
+                                padding=0,
+                                border=ft.Border(right=ft.BorderSide(2, "#B8B8C7")),
+                                width=content_window_width * 0.75 /5 -1 ,
+                                content=ft.Text("only monitoring",color="Black",font_family="Roboto",size=text_size,text_align=ft.TextAlign.CENTER)
+                            ),
+                            ft.Container(
+                                padding=0,
+                                border=ft.Border(right=ft.BorderSide(2, "#B8B8C7")),
+                                width=content_window_width * 0.75 /5 -1,
+                                content=ft.Text("not applicable",color="Black",font_family="Roboto",size=text_size,text_align=ft.TextAlign.CENTER)
+                            ),
+                            ft.Container(
+                                padding=0,
+                                border=ft.Border(right=ft.BorderSide(2, "#B8B8C7")),
+                                width=content_window_width * 0.75 /5 -1,
+                                content=ft.Text("not applicable",color="Black",font_family="Roboto",size=text_size,text_align=ft.TextAlign.CENTER)
+                            ),
+                            ft.Container(
+                                padding=0,
+                                border=ft.Border(right=ft.BorderSide(2, "#B8B8C7")),
+                                width=content_window_width * 0.75 /5 -1,
+                                content=ft.Text("none",color="Black",font_family="Roboto",size=text_size,text_align=ft.TextAlign.CENTER)
+                            )
+                        ]
+                    )
+                )
+            ]
+        )
     )
     column_array.append(container)
-    container = ft.Container(
+    '''container = ft.Container(
         padding=0,
         width=content_window_width * 0.75,
         height= content_window_height * 0.4,
         border=ft.border.all(1,ft.colors.YELLOW)
     )
-    column_array.append(container)
+    column_array.append(container)'''
     return column_array
     
 def washing_and_recovery_info(page):
+    #ft.TextSpan("",style=ft.TextStyle(size=text_size,font_family="Roboto",color=ft.colors.BLACK))
+    #ft.TextSpan("\n"),
     window_width = global_variables.app_window.width * 0.85
     window_height = global_variables.app_window.height * 0.9
     window_padding = window_width*0.01
